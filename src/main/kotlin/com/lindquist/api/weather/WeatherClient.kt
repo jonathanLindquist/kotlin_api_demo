93c79b3bca48f8daffb6e4cf0059e3c5d31486f5
jonathanLindquist <jonathan.lindquist@protonmail.com>
2022-05-24 11:24:58 -0500

    formatting

--- src/main/kotlin/com/lindquist/api/weather/WeatherClient.kt
+++ src/main/kotlin/com/lindquist/api/weather/WeatherClient.kt
@@ -1,19 +1,17 @@
 package com.lindquist.api.weather
 
-import com.lindquist.api.weather.models.*
+import com.lindquist.api.weather.models.CityWeather
+import com.lindquist.api.weather.models.GoWeatherDTO
+import com.lindquist.api.weather.models.toCityWeather
 import io.netty.channel.ChannelOption
-import io.netty.handler.codec.http.HttpResponseStatus
 import io.netty.handler.timeout.ReadTimeoutHandler
 import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientResponseException
-import org.springframework.web.reactive.function.client.WebClientResponseException.Unauthorized
 import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
 import java.nio.charset.StandardCharsets
@@ -21,39 +19,34 @@ import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 @Component
-class WeatherClient(
-    val client: WebClient = webClient()
-) {
+class WeatherClient(val client: WebClient = webClient()) {
   fun getWeather(city: String): Mono<CityWeather> =
-      client.post()
-          .uri("https://goweather.herokuapp.com/weather/${city}")
-          .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-          .accept(MediaType.APPLICATION_JSON)
-          .acceptCharset(StandardCharsets.UTF_8)
-          .retrieve()
-          .onStatus(HttpStatusCode::is4xxClientError) { response ->
-            if (response.statusCode() == HttpStatusCode.valueOf(403)) {
-              Mono.error(Exception())
-            } else {
-              Mono.error(Exception())
-            }
-          }
-          .toEntity(GoWeatherDTO::class.java)
-          .mapNotNull { it.body?.toCityWeather() }
+    client.post()
+      .uri("https://goweather.herokuapp.com/weather/${city}")
+      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON)
+      .acceptCharset(StandardCharsets.UTF_8)
+      .retrieve()
+      .onStatus(HttpStatusCode::is4xxClientError) { response ->
+        if (response.statusCode() == HttpStatusCode.valueOf(403)) {
+          Mono.error(Exception())
+        } else {
+          Mono.error(Exception())
+        }
+      }.toEntity(GoWeatherDTO::class.java)
+      .mapNotNull { it.body?.toCityWeather() }
 }
 
 fun webClient(): WebClient =
-    WebClient
-        .builder()
-        .clientConnector(ReactorClientHttpConnector(httpClient()))
-        .build()
+  WebClient.builder()
+    .clientConnector(ReactorClientHttpConnector(httpClient()))
+    .build()
 
 fun httpClient(): HttpClient =
-    HttpClient.create()
-        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-        .responseTimeout((Duration.ofMillis(5000)))
-        .doOnConnected { conn ->
-          conn
-              .addHandlerLast(ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-              .addHandlerLast(WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-        }
+  HttpClient.create()
+    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+    .responseTimeout((Duration.ofMillis(5000)))
+    .doOnConnected { conn ->
+      conn.addHandlerLast(ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+        .addHandlerLast(WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+    }
