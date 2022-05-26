e7843d6e409cbe299ec04df50186608ac0e7d874
jonathanLindquist <jonathan.lindquist@protonmail.com>
2022-05-26 12:32:30 -0500

    formatting

--- src/main/kotlin/com/lindquist/api/weather/WeatherController.kt
+++ src/main/kotlin/com/lindquist/api/weather/WeatherController.kt
@@ -1,7 +1,6 @@
 package com.lindquist.api.weather
 
 import com.lindquist.api.weather.models.CityWeatherDTO
-import com.lindquist.api.weather.models.toCityWeatherDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,6 +12,6 @@ class WeatherController(val weatherService: WeatherService) {
   @GetMapping("/city/{city}")
   fun getWeatherByCity(@PathVariable city: String, @RequestParam item: String?): Mono<CityWeatherDTO> {
     return weatherService
-        .getWeatherByCity(city)
+      .getWeatherByCity(city)
   }
 }
--- src/main/kotlin/com/lindquist/api/weather/WeatherService.kt
+++ src/main/kotlin/com/lindquist/api/weather/WeatherService.kt
@@ -1,6 +1,5 @@
 package com.lindquist.api.weather
 
-import com.lindquist.api.weather.models.CityWeather
 import com.lindquist.api.weather.models.CityWeatherDTO
 import com.lindquist.api.weather.models.toCityWeatherDTO
 import org.springframework.stereotype.Service
@@ -9,6 +8,6 @@ import reactor.core.publisher.Mono
 @Service
 class WeatherService(val client: WeatherClient) {
   fun getWeatherByCity(city: String): Mono<CityWeatherDTO> =
-      client.getWeather(city)
-          .mapNotNull { it?.toCityWeatherDTO() }
+    client.getWeather(city)
+      .mapNotNull { it?.toCityWeatherDTO() }
 }
